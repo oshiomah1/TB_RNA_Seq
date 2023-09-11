947f6e3e1454360da5c8c7d951853b8cb7e8efd5
oshiomah1 <71351553+oshiomah1@users.noreply.github.com>
2023-09-11 10:20:37 -0700

    restructuring folder

---
+++ deprecated/README.md
@@ -0,0 +1,49 @@
+# TB_RNA_Seq: Merging Datasets + Decision Trees
+
+## There are three R scripts in this project
+
+#### case_decision_tree.R: Assigns individuals as cases or other based on initial self-report and clinical syptoms and verified with current medical data
+
+#### control_decision_tree.R :Assigns individuals as controls based on PRIOR medical records. Searches a wide range of variables for TB evidence in their lifetime.
+
+#### updated_data_merge.R: It merges the smaller RNA seq study with the larger GWAS study using fuzzy name matching, saliva barcodes and manual matching. It automatically calls case_decision_tree.R and control_decision_tree.R
+
+#### \*\*raw_NCTB_data & raw_NCR_data
+
+# Before running Scripts:
+
+## Before running, you must
+
+### 1) In each script manually check that the datafile is the most recent from REDCAP, if not redownload. The variables where you edit these are
+
+#### a) control_decision_tree.R : raw_NCTB_TB_test_vars
+
+#### b) updated_data_merge.R: raw_NCTB_data & raw_NCR_data
+
+#### c) case_decision_tree.R: raw_NCR_data\_
+
+### 2) In updated_data_merge.R you must change output_path to your actual desired path on dropbox or your desktop (it's Oshi's by default)
+
+# Running Scripts:
+
+### There are 2 options to run the scripts
+
+### 1) Quick'n'easy mode: Only Run updated_data_merge.R. This script actually automatically sources both decision tree scripts. You should get an output pop up on your rstudio with final case/control counts
+
+### 2) Investigator mode: First you can run case_decision_tree and control_decision_tree in any order, then updated data merge. You would do this when trying to find cracks in the system.
+
+##### An important thing to track here is why some cases fall of the assignment tree. After running case_decision_tree , view(Validatedcases2) and check out these columns for individuals that have become notcase_other
+
+1.  lab_date_2_diag_date = lab_receive_date - TB_Diag_Date,
+
+2.  lab_date_2_trtment_strt = lab_receive_date - treatment_srt_date,
+
+3.  less_than_thirty = lab_receive_date - TB_Diag_Date \< 30,
+
+4.  intermediate2 = tb_status == "Yes",
+
+5.  intermediate3 = HIV_status == "Negative",
+
+### 
+
+*#to do: make another script from updated data_merge to check for missing demographic data*
---
+++ deprecated/case_decision_tree.R
@@ -0,0 +1,76 @@
+#case decision tree
+library(tidyverse)
+#vector to rename columns later
+NCR_new_name <- c(Surname = "Participant.Surname", First.Name = "Participant.first.name", NCR_Age = "Age.of.participant", NCR_Gender = "Sex..gender..of.participant.", NCR_Phleb_date ="Date.and.Time.of.Phlebotomy", NCR_Saliva = "Participant.study.saliva.ID", current_other_inf ="Do.you.currently.have.an.infection.such.as.a.cold.or.the.flu.....not.TB.", tb_status = "Do.you.currently.have.TB........", lab_receive_date = "Date.PBMC.received.in.the.Lab", HIV_status="What.is.your.HIV.status." , treatment_srt_date =  "TB.treatment.start.date." , TB_Diag_Date ="Date.of.TB.diagnosis.")
+
+#read in data (Thin report with data labels from Redcap)
+raw_NCR_data_ <- read.csv(
+  "/Users/oshi/Downloads/InvestigationOfAnces-ThinReport_DATA_LABELS_2023-08-31_0744.csv",
+  header = TRUE,
+  sep = ";",
+  na.strings = c("", " ", "NA", "N/A"),
+  colClasses = c("Participant.study.saliva.ID" = "character")
+) %>%
+  rename(all_of(NCR_new_name)) %>%
+  unite("NCR.Name", Surname, First.Name, sep = " ", remove = FALSE, na.rm = TRUE) %>%
+  mutate(treatment_srt_date = as.Date(treatment_srt_date),
+         lab_receive_date = as.Date(lab_receive_date), TB_Diag_Date= as.Date(as.Date(TB_Diag_Date)))
+
+pre_cases <- raw_NCR_data_ %>%
+  filter(tb_status == "Yes")
+
+# Extract the sample_id values from the filtered data into a list
+pre_cases_list <- pre_cases$Participant.study.ID
+#Date PBMC received in lab [NCR], TB test result [NCR], TB lab diagnosis date [NCR], treatment start date [NCR], HIV status self-report [NCR]
+
+
+case_function <- function(TB_Diag_Date, tb_status, HIV_status, treatment_srt_date, lab_receive_date) {
+  result <- case_when(
+    tb_status == "Yes"  ~
+      case_when(
+        lab_receive_date - TB_Diag_Date < 30 ~
+          case_when(
+            HIV_status == "Negative" ~
+              case_when(
+                !is.na(treatment_srt_date) &
+                  lab_receive_date - treatment_srt_date < 15 ~ "2weekCase",
+                !is.na(lab_receive_date) &
+                  lab_receive_date - TB_Diag_Date < 15 ~ "2weekCase",
+                TRUE ~ "STOP"
+              ),
+            TRUE ~ "STOP"
+          ),
+        TRUE ~ "STOP"
+      ),
+    TRUE ~ "STOP"
+  )
+  
+  return(result)
+}
+
+Validated_Cases <- raw_NCR_data_ %>%
+  mutate(case_status = case_function(TB_Diag_Date, tb_status, HIV_status, treatment_srt_date, lab_receive_date)) %>% filter(tb_status =="Yes") %>% mutate(validated_case_status = case_when(current_other_inf =="No" & case_status == "2weekCase" ~ "2weekCase", current_other_inf =="Yes" & case_status =="2weekCase"  ~ "2weekCasewithflu", is.na(current_other_inf) & case_status =="2weekCase" ~ "2weekCaseunknwnflu", TRUE ~"notcase_other"))
+
+final_case_key <- Validated_Cases %>% select(Participant.study.ID,validated_case_status)
+# Now Validated_Cases dataframe will have a new column called "val_cases" with the function results
+count_table <- as.data.frame(table(Validated_Cases$validated_case_status))
+
+
+# Rename the columns for clarity
+colnames(count_table) <- c("Validated_Case_Status", "Count")
+
+# Print the new dataframe with counts
+print(count_table)
+#write_csv(Validated_Cases,"/Users/oshi/Desktop/TB/Validated_Cases.csv")
+
+#sanity check
+
+Validated_Cases2 <- raw_NCR_data_ %>%
+  mutate(
+    lab_date_2_diag_date = lab_receive_date - TB_Diag_Date,
+    lab_date_2_trtment_strt = lab_receive_date - treatment_srt_date,
+    less_than_thirty = lab_receive_date - TB_Diag_Date < 30,
+    intermediate2 = tb_status == "Yes",
+    intermediate3 = HIV_status == "Negative",
+    val_cases = case_function(TB_Diag_Date, tb_status, HIV_status, treatment_srt_date, lab_receive_date)
+  ) %>% select(Participant.study.ID, tb_status,HIV_status,treatment_srt_date,lab_receive_date,TB_Diag_Date,val_cases,lab_date_2_diag_date:val_cases)
